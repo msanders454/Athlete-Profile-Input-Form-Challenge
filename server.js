f4a1b76bc67b1fc3d14644d19827ec7fe32c8122
Michael Sanders <mikesanderspsu@gmail.com>
2020-06-22 01:03:38 -0400

    Edited Server url

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var morgan = require('morgan');
 var mongoose = require('mongoose')
 var path = require('path');
 var bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
 const router = express.Router();
 require('./app/routes')(app);
 
@@ -16,14 +17,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(express.static(__dirname + '/public'));
 
+let uri = "mongodb://heroku_3hj7qs2b:mj1q8clap0f76hlfgjlvuvsbvu@ds253418.mlab.com:53418/heroku_3hj7qs2b";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  if (err){
+    console.log("Not Connected to the DB " + err)
+} else {
+    console.log('Success')
+}
+  client.close();
+});
 
-mongoose.connect('mongodb://localhost:27017/athlete', function(err){
-    if (err){
-        console.log("Not Connected to the DB " + err)
-    } else {
-        console.log('Success')
-    }
-})
 
 app.use(router);
 
