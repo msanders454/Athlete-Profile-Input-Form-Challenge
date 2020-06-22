27b8e67291fd53f9b9f280aff361848f631974f5
Michael Sanders <mikesanderspsu@gmail.com>
2020-06-22 01:08:49 -0400

    Edited Server url

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var morgan = require('morgan');
 var mongoose = require('mongoose')
 var path = require('path');
 var bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient;
 const router = express.Router();
 require('./app/routes')(app);
 
@@ -18,17 +17,13 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(express.static(__dirname + '/public'));
 
 let uri = "mongodb://heroku_3hj7qs2b:mj1q8clap0f76hlfgjlvuvsbvu@ds253418.mlab.com:53418/heroku_3hj7qs2b";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  if (err){
-    console.log("Not Connected to the DB " + err)
-} else {
-    console.log('Success')
-}
-  client.close();
-});
-
+MongoClient.connect(uri, { useNewUrlParser: true }, function(err){
+    if (err){
+        console.log("Not Connected to the DB " + err)
+    } else {
+        console.log('Success')
+    }
+})
 
 app.use(router);
 
